f1587c5dc6a8d110586e21deced50f33572254cf
ManuelaCorte <manu.cortepause@gmail.com>
2024-02-16 16:36:26 +0100

    New functionalities: - added dependecies management - added pre-commit hooks - add data examples - added tests for naive intersection

---
+++ tests/test_naive_intersection.py
@@ -0,0 +1,13 @@
+import os
+from src.data import read_intersection_data
+from src.intersection import naive_intersection
+
+
+# Test files were taken from: https://github.com/the-hyp0cr1t3/sweepline-intersections
+def test_naive_intersection():
+    files = os.listdir("data/intersections")
+    for file in files:
+        path = f"data/intersections/{file}"
+        print(f"Testing {path}")
+        segments, num_intersections = read_intersection_data(path)
+        assert len(naive_intersection(segments)) == num_intersections
